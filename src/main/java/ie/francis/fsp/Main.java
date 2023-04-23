96cba5346790b1f9e1e5227fd7d36be11a670eef
Francis McNamee <francis.mcnamee@icloud.com>
2023-04-23 02:54:10 +0100

    Removed test class

--- src/main/java/ie/francis/fsp/Main.java
+++ src/main/java/ie/francis/fsp/Main.java
@@ -7,21 +7,14 @@ package ie.francis.fsp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-  public static void main(String[] args)
-      throws IOException,
-          ClassNotFoundException,
-          NoSuchMethodException,
-          IllegalAccessException,
-          InvocationTargetException,
-          InstantiationException {
+  public static void main(String[] args) throws IOException {
 
     Repl repl = new Repl();
     while (true) {
       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-      System.out.print(">> ");
+      System.out.print("> ");
       String input = reader.readLine();
       if (input.equalsIgnoreCase("exit")) {
         break;
