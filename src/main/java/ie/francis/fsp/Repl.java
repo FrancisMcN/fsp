4f4e7bf2b00baeb8e61ffddcbf93f6ed4d6b7940
Francis McNamee <francis.mcnamee@icloud.com>
2023-05-25 00:24:35 +0100

    Added example code generation

--- src/main/java/ie/francis/fsp/Repl.java
+++ src/main/java/ie/francis/fsp/Repl.java
@@ -7,9 +7,12 @@ package ie.francis.fsp;
 import ie.francis.fsp.ast.Node;
 import ie.francis.fsp.ast.Visitor;
 import ie.francis.fsp.ast.VisitorImpl;
+import ie.francis.fsp.classloader.CustomClassLoader;
+import ie.francis.fsp.codegen.ClassGenerator;
 import ie.francis.fsp.exception.SyntaxErrorException;
 import ie.francis.fsp.parser.Parser;
 import ie.francis.fsp.scanner.Scanner;
+import java.lang.reflect.InvocationTargetException;
 
 public class Repl {
   public void eval(String value) {
@@ -22,7 +25,19 @@ public class Repl {
       Node ast = parser.parse();
       Visitor visitor = new VisitorImpl();
       ast.accept(visitor);
-    } catch (SyntaxErrorException ex) {
+      ClassGenerator generator = new ClassGenerator("testclass");
+      byte[] bytes = generator.generate();
+      //      try (FileOutputStream fos = new FileOutputStream("test.class")) {
+      //        fos.write(bytes);
+      //      }
+
+      //      System.out.println(Arrays.toString(bytes));
+      Class c = new CustomClassLoader().defineClass("ie.francis.testclass", bytes);
+      c.getMethod("main").invoke(null);
+    } catch (SyntaxErrorException
+        | NoSuchMethodException
+        | IllegalAccessException
+        | InvocationTargetException ex) {
       ex.printStackTrace();
     }
   }
---
+++ src/main/java/ie/francis/fsp/classloader/CustomClassLoader.java
@@ -0,0 +1,11 @@
+/*
+ * (c) 2023 Francis McNamee
+ * */
+
+package ie.francis.fsp.classloader;
+
+public class CustomClassLoader extends ClassLoader {
+  public Class defineClass(String name, byte[] b) {
+    return defineClass(name, b, 0, b.length);
+  }
+}
