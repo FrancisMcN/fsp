1c6540777fc8a95fa1674b5039065bbc409f982b
Francis McNamee <francis.mcnamee@ibm.com>
2024-01-18 15:39:30 +0000

    Changed type method to return Java type strings

--- src/main/java/ie/francis/lisp/function/Type.java
+++ src/main/java/ie/francis/lisp/function/Type.java
@@ -5,9 +5,7 @@
 package ie.francis.lisp.function;
 
 import ie.francis.lisp.exception.NotImplementedException;
-import ie.francis.lisp.type.Cons;
 import ie.francis.lisp.type.Lambda;
-import ie.francis.lisp.type.Symbol;
 
 public class Type implements Lambda {
   @Override
@@ -17,20 +15,8 @@ public class Type implements Lambda {
 
   @Override
   public Object call(Object arg) {
-    if (arg instanceof Cons) {
-      return "cons";
-    } else if (arg instanceof Lambda) {
-      return "lambda";
-    } else if (arg instanceof Symbol) {
-      return "symbol";
-    } else if (arg instanceof Integer) {
-      return "integer";
-    } else if (arg instanceof Float) {
-      return "float";
-    } else if (arg instanceof Boolean) {
-      return "bool";
-    } else if (arg instanceof String) {
-      return "string";
+    if (arg != null) {
+      return arg.getClass().getName();
     }
     return null;
   }
